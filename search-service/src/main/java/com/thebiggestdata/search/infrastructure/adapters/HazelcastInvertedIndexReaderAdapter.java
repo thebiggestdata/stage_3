a144b8af627b4e145827cf2ec969da6597d4520c
juand4569 <juand24601@gmail.com>
2025-12-02 11:16:14 +0100

    feat(adapter): search service hazelcast adapter

---
+++ search-service/src/main/java/com/thebiggestdata/search/infrastructure/adapters/HazelcastInvertedIndexReaderAdapter.java
@@ -0,0 +1,26 @@
+package com.thebiggestdata.search.infrastructure.adapters;
+
+import com.thebiggestdata.search.domain.port.InvertedIndexReaderPort;
+import com.hazelcast.core.HazelcastInstance;
+import com.hazelcast.multimap.MultiMap;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class HazelcastInvertedIndexReaderAdapter implements InvertedIndexReaderPort {
+
+    private final HazelcastInstance hazelcast;
+    private final MultiMap<String, Integer> index;
+
+    public HazelcastInvertedIndexReaderAdapter(HazelcastInstance hazelcast, String mapName) {
+        this.hazelcast = hazelcast;
+        this.index = hazelcast.getMultiMap(mapName);
+    }
+
+    @Override
+    public List<Integer> getBookIdsForToken(String token) {
+        var values = index.get(token);
+
+        return values == null ? List.of() : new ArrayList<>(values);
+    }
+}

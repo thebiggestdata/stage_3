0a7bf97d09c6dafb498dab8871a5283b8c614ffa
juand4569 <juand24601@gmail.com>
2025-12-02 11:49:01 +0100

    feat(app): main for the search service

---
+++ search-service/src/main/java/com/thebiggestdata/search/App.java
@@ -0,0 +1,41 @@
+package com.thebiggestdata.search;
+
+import com.hazelcast.client.HazelcastClient;
+import com.hazelcast.client.config.ClientConfig;
+import com.hazelcast.core.HazelcastInstance;
+
+import com.thebiggestdata.search.application.usecase.SearchBookUseCase;
+import com.thebiggestdata.search.domain.service.SearchEngine;
+import com.thebiggestdata.search.infrastructure.adapter.HazelcastInvertedIndexReaderAdapter;
+import com.thebiggestdata.search.infrastructure.controller.SearchController;
+
+import io.javalin.Javalin;
+
+public class App {
+
+    public static void main(String[] args) {
+
+        System.out.println("Starting Search Service...");
+
+        ClientConfig config = new ClientConfig();
+        config.setClusterName("biggestdata-cluster");
+
+        HazelcastInstance hazelcast = HazelcastClient.newHazelcastClient(config);
+
+        var indexReader = new HazelcastInvertedIndexReaderAdapter(
+                hazelcast,
+                "inverted-index"
+        );
+
+        var searchEngine = new SearchEngine();
+
+        var searchUseCase = new SearchBookUseCase(indexReader, searchEngine);
+
+        Javalin app = Javalin.create().start(8080);
+
+        var controller = new SearchController(searchUseCase);
+        controller.registerRoutes(app);
+
+        System.out.println("Search Service started on port 8080");
+    }
+}

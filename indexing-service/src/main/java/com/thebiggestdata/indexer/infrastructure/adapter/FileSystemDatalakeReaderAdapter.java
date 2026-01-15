bf974b51687600d3230dfd82e53da9034c9c03e7
d4rk-h <herreragp334@gmail.com>
2026-01-15 19:54:27 +0000

    fix: omagat

--- indexing-service/src/main/java/com/thebiggestdata/indexer/infrastructure/adapter/FileSystemDatalakeReaderAdapter.java
+++ indexing-service/src/main/java/com/thebiggestdata/indexer/infrastructure/adapter/FileSystemDatalakeReaderAdapter.java
@@ -3,15 +3,20 @@ package com.thebiggestdata.indexer.infrastructure.adapter;
 import com.thebiggestdata.indexer.domain.model.RawDocument;
 import com.thebiggestdata.indexer.domain.port.DatalakeReadPort;
 import java.nio.file.Files;
+import java.nio.file.NoSuchFileException;
 import java.nio.file.Path;
 
 public class FileSystemDatalakeReaderAdapter implements DatalakeReadPort {
 
     @Override
     public RawDocument read(int bookId, String bodyPath) {
+        System.err.println("Intentando leer: {}"+ bodyPath);
         try {
             String body = Files.readString(Path.of(bodyPath));
             return new RawDocument(bookId, body);
-        } catch (Exception e) {throw new RuntimeException("Failed to read " + bodyPath, e);}
+        } catch (Exception e) {
+            throw new RuntimeException("Failed to read " + bodyPath, e);
+        }
     }
+
 }
--- indexing-service/src/main/java/com/thebiggestdata/indexer/infrastructure/adapter/LocalDatalakePathResolver.java
+++ indexing-service/src/main/java/com/thebiggestdata/indexer/infrastructure/adapter/LocalDatalakePathResolver.java
@@ -1,16 +1,18 @@
 package com.thebiggestdata.indexer.infrastructure.adapter;
 
 import com.thebiggestdata.indexer.domain.port.DatalakePathResolverPort;
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
 
 public class LocalDatalakePathResolver implements DatalakePathResolverPort {
     private final String basePath;
 
     public LocalDatalakePathResolver() {
-        this.basePath = System.getenv().getOrDefault("DATALAKE_BASE_PATH", "/app/datalake");
+        this.basePath = "/app/datalake" + LocalDate.now().format(DateTimeFormatter.BASIC_ISO_DATE);
     }
 
     @Override
     public String resolve(int bookId) {
-        return basePath + "/" + bookId + ".txt";
+        return basePath + "/" + bookId + "_body.txt";
     }
 }

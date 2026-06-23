9a2517ec648ead8e37acb9198424d22cea1ad040
juand4569 <juand24601@gmail.com>
2026-06-23 11:53:47 +0200

    refactor(ingestion): remove legacy ingestion ports

--- ingestion-service/src/main/java/com/thebiggestdata/ingestion/infrastructure/ports/BookIngestedNotifierPort.java
+++
@@ -1,7 +0,0 @@
-package com.thebiggestdata.ingestion.infrastructure.ports;
-
-import com.thebiggestdata.ingestion.model.BookIngestedEvent;
-
-public interface BookIngestedNotifierPort {
-    void notify(BookIngestedEvent event); //TODO maybe change it to 'public' (to the broker)
-}
--- ingestion-service/src/main/java/com/thebiggestdata/ingestion/infrastructure/ports/BookProviderPort.java
+++
@@ -1,7 +0,0 @@
-package com.thebiggestdata.ingestion.infrastructure.ports;
-
-import com.thebiggestdata.ingestion.model.BookContent;
-
-public interface BookProviderPort {
-    BookContent getBookContent(int bookId);
-}
